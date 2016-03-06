f6cb003a456d4e06a3f160456018bf2a4d1bc5a2
Patrick Arlt <patrick.arlt@gmail.com>
2016-03-06 08:30:42 -0800

    first draft

---
+++ demo/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('css', function () {
+  return gulp.src('*.scss')
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'expanded'
+    }))
+    .pipe(postcss([
+      autoprefixer({browsers: ['last 2 versions']})
+    ]))
+    .pipe(cleanCSS({debug: true}, function(details) {
+      console.log(details.name + ' Orginal Size: ' + details.stats.originalSize);
+      console.log(details.name + ' Minified Size: ' + details.stats.minifiedSize);
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./'))
+});
